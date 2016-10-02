bf7ebd6d52c21291e55dd3cbf83e07ba6ced254a
Jordan Doyle <jordan@doyle.wf>
2016-10-02 22:23:59 +0100

    Initial commit

---
+++ config/templates.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Wordpress Templates
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set all the Wordpress templates that will be used in this
+    | application. This should be in slug => friendly name format.
+    */
+
+    'home' => 'Homepage'
+
+];
+
