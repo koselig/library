b33e69fbbba1a7841d2febc0fffe423d5cbe5a39
Jordan Doyle <jordan@doyle.wf>
2016-10-16 12:18:34 +0100

    Use new request file

---
+++ src/Http/Request.php
@@ -0,0 +1,29 @@
+<?php
+namespace Koselig\Http;
+
+use Illuminate\Http\Request as BaseRequest;
+use Koselig\Models\Post;
+use Koselig\Support\Wordpress;
+
+/**
+ * Extend the Request class to add some Wordpress-related helpers.
+ *
+ * @author Jordan Doyle <jordan@doyle.wf>
+ */
+class Request extends BaseRequest
+{
+    /**
+     * @var Post
+     */
+    private $post;
+
+    /**
+     * Get the Post instance this request has asked for.
+     *
+     * @return Post
+     */
+    public function post()
+    {
+        return $this->post ?: $this->post = Post::find(Wordpress::id());
+    }
+}
