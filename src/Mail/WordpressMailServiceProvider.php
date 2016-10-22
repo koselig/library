6bb8f14d1823cf13d0f7f640f2e24c8c0d88c38e
Jordan Doyle <jordan@doyle.wf>
2016-10-22 20:04:17 +0100

    Add gravityform directive & fix 404 on get routes that dont exist inside wordpress

--- src/Mail/WordpressMailServiceProvider.php
+++ src/Mail/WordpressMailServiceProvider.php
@@ -4,7 +4,7 @@ namespace Koselig\Mail;
 use Illuminate\Support\ServiceProvider;
 
 /**
- *
+ * Override Wordpress' wp_mail function to use the Laravel mailer.
  *
  * @author Jordan Doyle <jordan@doyle.wf>
  */
