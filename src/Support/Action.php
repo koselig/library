533362dd6f161970113e4d32ace8ff37c2818f81
Jordan Doyle <jordan@doyle.wf>
2016-11-18 21:56:07 +0000

    Allow registering hooks before wordpress has been loaded

--- src/Support/Action.php
+++ src/Support/Action.php
@@ -18,6 +18,20 @@ class Action
      */
     public static function hook($tag, $function, $priority = 10, $acceptedArgs = 1)
     {
+        if (!function_exists('add_filter')) {
+            // we need to register this hook before wp-settings.php is included, which also means
+            // that add_filter hasn't been included yet so we have to add to the wp_filter global
+            // manually.
+            $GLOBALS['wp_filter'][$tag][$priority][] = [
+                'function' => function (...$args) use ($function) {
+                    return app()->call($function, $args);
+                },
+                'accepted_args' => $acceptedArgs,
+            ];
+
+            return;
+        }
+
         add_filter($tag, function (...$args) use ($function) {
             return app()->call($function, $args);
         }, $priority, $acceptedArgs);
